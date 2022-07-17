416b9045891b48b1a83178eb49a4ed13ba7032e0
jayce <jslesar@beta.team>
2022-07-17 17:17:00 -0400

    ok

--- blink/main.c
+++ blink/main.c
@@ -1,18 +1,17 @@
 #include "pico/stdlib.h"
-#include <stdbool.h>
-
-const unsigned int LED_PIN = 25;
 
 int main() {
-
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#else
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-
     while (true) {
-        gpio_put(LED_PIN, true);
-        sleep_ms(1000);
-        gpio_put(LED_PIN, false);
-        sleep_ms(1000);
-
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
     }
+#endif
 }
