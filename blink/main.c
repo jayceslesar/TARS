5c59b7d1cd17a253b271aa52c965484fb28ed502
jayce <jslesar@beta.team>
2022-07-17 17:18:57 -0400

    cleaner

--- blink/main.c
+++ blink/main.c
@@ -1,9 +1,7 @@
 #include "pico/stdlib.h"
 
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
+
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -13,5 +11,4 @@ int main() {
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
     }
-#endif
 }
